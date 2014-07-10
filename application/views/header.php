daf1aec52697e67bf64f73740b9254693b9451d2
asartawi <abd.sarta@gmail.com>
2014-07-11 01:30:46 +0300

    landing page header

---
+++ application/views/header.php
@@ -0,0 +1,33 @@
+<!--<div class="navbar navbar-fixed-top">-->
+<!--    <div class="navbar-inner">-->
+<div style = "background-color: lightgray">
+        <div class="container" >
+            <img src="images/palpac_logo.png" style="max-width: 300px; margin: 7px;">
+        </div>
+<!--    </div>-->
+
+</div>
+<nav id="myNavbar" class="navbar navbar-default" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Pal Pac</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Machines</a></li>
+                <li><a href="#">About us </a></li>
+                <li><a href="#">Contact us</a></li>
+            </ul>
+
+        </div><!-- /.navbar-collapse -->
+    </div>
+</nav>
